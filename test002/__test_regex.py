0f9c27011273e68cc15464d79464158652f21883
Jason <2227800977@qq.com>
2017-01-17 13:16:22 +0800

    正则表达式

--- test002/__test_regex.py
+++ test002/__test_regex.py
@@ -10,3 +10,12 @@ print re.findall("top", st)
 res = r"t[^io]p"
 
 print re.findall(res, st)
+
+st = "hello a hello b"
+print re.findall("^hello", st)
+print "====", re.findall("hello$", st)
+
+res = "t[abc$]"
+
+res = r"x[a-zA-Z0-9]{3}y"
+print "====", re.findall(res, "x123y")
