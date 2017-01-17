915ef757562d5a3fd7fea4c30d88681cbad6f8a0
Jason <2227800977@qq.com>
2017-01-17 13:59:15 +0800

    文件读写

---
+++ test003/__test__file_open.py
@@ -0,0 +1,15 @@
+# coding:utf8
+
+# 打开文件
+# fo = open("test.txt", "w+")
+# fo = open("test.txt", "r+")
+fo = open("test.txt", "a+")
+# 读取文件内容
+fo.write("123")
+# flush数据
+fo.flush()
+content = fo.read()
+# 关闭文件
+fo.close()
+
+print content
