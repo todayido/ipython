e70a7c8d344223cf3cf40e8b9064de9728a33f90
Jason <2227800977@qq.com>
2017-01-17 15:05:51 +0800

    文件目录操作

---
+++ test003/__test_file_mkdir.py
@@ -0,0 +1,16 @@
+# coding:utf8
+import os
+
+# 创建目录
+# os.mkdir("test")
+
+# os.makedirs("test/test1/test2")
+
+# 删除多级目录
+# os.removedirs("test/test1/test2")
+
+# 打印当前目录
+print os.getcwd()
+
+print os.listdir("../test003")
+
