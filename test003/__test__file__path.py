ffa692fb07da9238924077947a761dd5baca37c4
Jason <2227800977@qq.com>
2017-01-17 15:24:28 +0800

    递归遍历文件路径下所有的文件

---
+++ test003/__test__file__path.py
@@ -0,0 +1,15 @@
+# coding:utf8
+import os
+
+
+def __print__ath(path):
+    for __file_name in os.listdir(path):
+        # __file_path = os.getcwd() + "\\" + __file_name
+        __file_path = os.path.join(path, __file_name)
+
+        if os.path.isdir(__file_path) == True:
+            __print__ath(__file_path)
+        print __file_path
+
+
+__print__ath("../test003")
